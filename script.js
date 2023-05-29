5f59f6990a56a3332953821f90597ba8fab74920
Fernando Jimenez <fgjv06@gmail.com>
2023-05-29 18:33:37 -0400

    JS - activateEyeDropper function

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //Getting variables
 const colorPickerBtn = document.querySelector("#color-picker");
 
+const activateEyeDropper = () => {
+  
+}
+
 colorPickerBtn.addEventListener("click", activateEyeDropper);
