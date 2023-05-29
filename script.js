c12c841af30b27bfe79a68ee23af1dcf22643620
Fernando Jimenez <fgjv06@gmail.com>
2023-05-29 19:15:22 -0400

    JS - const response

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //Getting variables
 const colorPickerBtn = document.querySelector("#color-picker");
 
-const activateEyeDropper = () => {
+const activateEyeDropper = async () => {
   try {
-    const eyeDropper = new eyeDropper(); // Creating a new eye dropper object. It's used to select colors from the screen
+    const eyeDropper = new EyeDropper(); // Creating a new eye dropper object. It's used to select colors from the screen
+    const response = await eyeDropper.open();
+    console.log(response)
   } catch(error) {
     console.log(error);
   }
