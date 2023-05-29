9dd9e3243b55020b3cc7452d378efea4a3b8f9b6
Fernando Jimenez <fgjv06@gmail.com>
2023-05-29 18:35:50 -0400

    JS - activateEyeDropper try (creating a new eye dropper object. It's used to select colors from the screen) and console.log if error

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 const colorPickerBtn = document.querySelector("#color-picker");
 
 const activateEyeDropper = () => {
-  
+  try {
+    const eyeDropper = new eyeDropper(); // Creating a new eye dropper object. It's used to select colors from the screen
+  } catch(error) {
+    console.log(error);
+  }
 }
 
 colorPickerBtn.addEventListener("click", activateEyeDropper);
