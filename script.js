d9c96675668aa68a5e88725dd5e0a5931fcbc365
Fernando Jimenez <fgjv06@gmail.com>
2023-05-30 18:55:53 -0400

    JS - copying the selected color to the clipboard

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const activateEyeDropper = async () => {
   try {
     const eyeDropper = new EyeDropper(); // Creating a new eye dropper object. It's used to select colors from the screen
     const { sRGBHex } = await eyeDropper.open();
+    navigator.clipboard.writeText(sRGBHex);
     console.log(sRGBHex)
   } catch(error) {
     console.log(error);
