74ca00e3e71b3b07bb6d6ba77324b5aa9de238a2
Fernando Jimenez <fgjv06@gmail.com>
2023-05-30 19:01:15 -0400

    JS - Selected color added to the pickedColors array

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ const activateEyeDropper = async () => {
     const eyeDropper = new EyeDropper(); // Creating a new eye dropper object. It's used to select colors from the screen
     const { sRGBHex } = await eyeDropper.open(); // Waiting to open the eyeDropper to get the color
     navigator.clipboard.writeText(sRGBHex); // Copy the selected color to the clipboard
-    console.log(sRGBHex)
+
+    pickedColors.push(sRGBHex); // Selected color added to the pickedColors array
+    console.log(pickedColors);
   } catch(error) {
     console.log(error);
   }
